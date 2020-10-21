306e67349bb0924174a5bf1ddb3862315d365c97
sameena83 <sameenah.tm@gmail.com>
2020-10-21 10:25:42 +0200

    Added tests

---
+++ src/test/java/moneytracker/com/IncomeTest.java
@@ -0,0 +1,32 @@
+package moneytracker.com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IncomeTest {
+
+    @Test
+    void getDate() {
+    }
+
+    @Test
+    void setDate() {
+    }
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void setAmount() {
+    }
+
+    @Test
+    void getIncomeType() {
+    }
+
+    @Test
+    void setIncomeType() {
+    }
+}
---
+++ src/test/java/moneytracker/com/ReportServiceTest.java
@@ -0,0 +1,32 @@
+package moneytracker.com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportServiceTest {
+
+    @Test
+    void calculateMonthlyExpense() {
+    }
+
+    @Test
+    void calculateYearlyExpense() {
+    }
+
+    @Test
+    void calculateYearlyIncome() {
+    }
+
+    @Test
+    void calculateCategoryExpense() {
+    }
+
+    @Test
+    void calculateMonthlyIncome() {
+    }
+
+    @Test
+    void calculateCategoryIncome() {
+    }
+}
